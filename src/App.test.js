0319b286113b9c931835a9c2285d10281ad92af3
celina520524 <celina520524@163.com>
2021-12-23 22:15:14 +0800

    update: mock request

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,49 @@
-import { render, screen } from '@testing-library/react'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { render, screen, fireEvent } from './test-utils'
 import userEvent from '@testing-library/user-event'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './Store'
-import configureMockStore from 'redux-mock-store'
 
+const todos = [
+  {
+    cid: 1,
+    title: '吃饭',
+    isCompleted: false
+  },
+  {
+    cid: 2,
+    title: '睡觉',
+    isCompleted: false
+  },
+  {
+    cid: 3,
+    title: '打豆豆',
+    isCompleted: false
+  }
+]
+
+export const handlers = [
+  rest.get('http://localhost:5000/todos', (req, res, ctx) => {
+   return res(
+     ctx.status(200),
+     ctx.json(todos),
+     ctx.delay(150)
+   )
+  })
+]
+const server = setupServer(...handlers)
+
+beforeAll(() => server.listen())
 beforeEach(() => {
-  render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  return render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   )
 })
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 describe('添加任务', () => {
   test('输入内容敲回车，内容被添加到任务列表中，输入框被清空', () => {
@@ -30,30 +59,61 @@ describe('添加任务', () => {
   })
 })
 
-describe('切换单个任务完成状态', () => {
-  test('切换任务完成状态按钮，任务的完成样式也跟随改变', () => {
-
-    const todoItem = screen.getByTestId('todo-item')
-    const todoDone = screen.getByTestId('todo-done')
-
-    expect(todoDone.checked).toBeFalsy()
-    expect(todoItem).not.toHaveClass('completed')
-    // expect(todoItem.classList.contains('completed')).toBeFalsy()
-
-    userEvent.click(todoDone)
-    expect(todoDone.checked).toBeTruthy()
-    expect(todoItem).toHaveClass('completed')
-  })
-})
-
 describe('删除任务', () => {
-  test('点击删除按钮，任务项应该被移除', () => {
+  test('点击删除按钮，任务项应该被移除', async () => {
+    await fetch('http://localhost:5000/todos', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    
     // 找到删除按钮
-    const button = screen.getByTestId('destroy')
+    const buttons = screen.getAllByTestId('destroy')
     // 点击删除按钮
-    userEvent.click(button)
+    userEvent.click(buttons[0])
     // 断言：
     //   页面中没有删除的任务项了
     expect(screen.queryByText('吃饭')).toBeNull()
+    //   余下任务项为2
+    const todoItems = screen.getAllByTestId('todo-item')
+    expect(todoItems.length).toBe(2)
+  })
+})
+
+describe('切换单个任务完成状态', () => {
+  test('切换任务完成状态按钮，任务的完成样式也跟随改变', async () => {
+
+    await fetch('http://localhost:5000/todos', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    // 任务项
+    const todoItems = screen.getAllByTestId('todo-item')
+    // 任务状态按钮
+    const todoDones = screen.getAllByTestId('todo-done')
+
+    // 初始任务状态断言
+    todoDones.forEach(t => {
+      expect(t.checked).toBeFalsy()
+    })
+    // 遍历任务项
+    todoItems.forEach(t => {
+      // 断言：
+      //    每项任务状态都不包含类名 completed
+      expect(t).not.toHaveClass('completed')
+      expect(t.classList.contains('completed')).toBeFalsy()
+    })
+    // 点击第一项任务，修改任务状态
+    userEvent.click(todoDones[0])
+    
+    // 断言：
+    //    第一项任务状态为 true
+    expect(todoDones[0].checked).toBeTruthy()
+    //    第一项任务包含类名 completed
+    expect(todoItems[0]).toHaveClass('completed')
   })
 })
